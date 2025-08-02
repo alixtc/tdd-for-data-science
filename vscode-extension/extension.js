962582b2b21ac0c62234c50a82f6bf822443f450
alixtc <alix.tiran-cappello@laposte.net>
2025-08-02 23:29:27 +0200

    Improve messages with simple layout and titles

--- vscode-extension/extension.js
+++ vscode-extension/extension.js
@@ -1,26 +1,38 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let showGoodNotification = vscode.commands.registerCommand('jupyter-notifier.showGoodNotification', function (message="✅ - Test Passed 🥳") {
-        vscode.window.showInformationMessage(message);
-    });
-    let showBadNotification = vscode.commands.registerCommand('jupyter-notifier.showBadNotification', function (message="❌ - Test Failed 😭") {
-        vscode.window.showInformationMessage(message);
-    });
+  let showGoodNotification = vscode.commands.registerCommand(
+    "jupyter-notifier.showGoodNotification",
+    function () {
+      vscode.window.showInformationMessage(
+        (message = "Good job!"),
+        (items = { title: "✅ - Test Passed 🎉" })
+      );
+    }
+  );
+  let showBadNotification = vscode.commands.registerCommand(
+    "jupyter-notifier.showBadNotification",
+    function () {
+      vscode.window.showInformationMessage(
+        (message = "Try Again!"),
+        (items = { title: "❌ - Test Failed 😭" })
+      );
+    }
+  );
 
-    context.subscriptions.push(showGoodNotification);
-    context.subscriptions.push(showBadNotification);
+  context.subscriptions.push(showGoodNotification);
+  context.subscriptions.push(showBadNotification);
 }
 
 function deactivate() {
-    vscode.commands.unregisterCommand('jupyter-notifier.showGoodNotification');
-    vscode.commands.unregisterCommand('jupyter-notifier.showBadNotification');
+  vscode.commands.unregisterCommand("jupyter-notifier.showGoodNotification");
+  vscode.commands.unregisterCommand("jupyter-notifier.showBadNotification");
 }
 
 module.exports = {
-    activate,
-    deactivate
-}
+  activate,
+  deactivate,
+};
