76b5d6f425186b0e650846a226ef06485a9b2e0c
alixtc <alix.tiran-cappello@laposte.net>
2025-08-02 11:04:34 +0200

    Working extension with 2 commands one good and one bad

--- vscode-extension/extension.js
+++ vscode-extension/extension.js
@@ -4,14 +4,21 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let showNotification = vscode.commands.registerCommand('jupyter-notifier.showNotification', function (message="This is the default example!") {
-        vscode.window.showWarningMessage(message);
+    let showGoodNotification = vscode.commands.registerCommand('jupyter-notifier.showGoodNotification', function (message="✅ - Test Passed 🥳") {
+        vscode.window.showInformationMessage(message);
+    });
+    let showBadNotification = vscode.commands.registerCommand('jupyter-notifier.showBadNotification', function (message="❌ - Test Failed 😭") {
+        vscode.window.showInformationMessage(message);
     });
 
-    context.subscriptions.push(showNotification);
+    context.subscriptions.push(showGoodNotification);
+    context.subscriptions.push(showBadNotification);
 }
 
-function deactivate() {}
+function deactivate() {
+    vscode.commands.unregisterCommand('jupyter-notifier.showGoodNotification');
+    vscode.commands.unregisterCommand('jupyter-notifier.showBadNotification');
+}
 
 module.exports = {
     activate,
